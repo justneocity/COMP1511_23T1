d377093bd5ef93e0d716fd6fd214c95e92b59a2a
Rifa Jamal <z5311190@ad.unsw.edu.au>
2023-02-17 10:47:09 +1100

    week01

---
+++ cat.c
@@ -0,0 +1,15 @@
+// Basic Program to print out cat
+// F09A 17th Feb 2023
+
+#include <stdio.h>
+
+int main (void) {
+
+    // printf(" /\\___/\\");
+    // printf("(=' w '=)\n");
+
+    printf(" /\___/\\n"
+          "(=' w '=)\n");
+    
+    return 0;
+}
